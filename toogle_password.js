2e686adc5cda45543b62b6ae9218d31bddc55c18
Awais Khan <awaiskhanniazi2233@gmail.com>
2026-06-28 15:23:29 +0500

    hide view pasword toggle added

---
+++ toogle_password.js
@@ -0,0 +1,13 @@
+function toggle(){
+    const passwordField=document.getElementById("password");
+    const eyeIcon=document.getElementById("eye")
+    if(passwordField.type === "password"){
+        passwordField.type="text";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");  
+      } else {
+        passwordField.type = "password"; 
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+      }
+}
